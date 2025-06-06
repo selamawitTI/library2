ef7c8d229bd43b02dec1f1ef6d095afcb113f818
selmawit tilahun <1selmawit1@gmail.com>
2025-06-06 18:03:36 +0300

    initial commit

---
+++ Author.java
@@ -0,0 +1,18 @@
+import java.util.Vector;
+
+public class Author {
+
+  public Integer name;
+
+  public iwrite bio;
+
+    public Vector  myBook;
+    public Vector  myLoan;
+
+  private void borrowbook() {
+  }
+
+  public void returnbook() {
+  }
+
+}
