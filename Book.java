337b0fff5f71e1db9c11ba44ae125960dba48094
selmawit tilahun <1selmawit1@gmail.com>
2025-06-06 19:35:00 +0300

    Initial commit

---
+++ Book.java
@@ -0,0 +1,28 @@
+import java.util.Vector;
+
+public class Book {
+
+  public SETP title;
+
+  public 1234 ISBN;
+
+  public Students publisher;
+
+  public 2017 year;
+
+    public Vector  myAuthor;
+    public Vector  myLibrarian;
+
+  private void gettitle() {
+  }
+
+  private void getISBN() {
+  }
+
+  public void getpublisher() {
+  }
+
+  public void getyear() {
+  }
+
+}
---
+++ Librarian.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class Librarian {
+
+  private 1234 librarianId;
+
+  private They/Them name;
+
+    public Vector  myBook;
+    public Vector  myLoan;
+    public Vector  myAuthor;
+
+  public void addbook() {
+  }
+
+  public void removebook() {
+  }
+
+  public void manageloan() {
+  }
+
+}
